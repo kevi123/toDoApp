8b16c434213fc48d6f10fae73292da46bb8baee4
Kevin Hinojosa <kevi123321@gmail.com>
2018-08-07 18:00:58 -0700

    dom

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+//alert()
+//confirm()//asks for bool
+//prompt()//ask for input
+
+
+//console.dir(document)//this give us all the info of the document
+console.log(document.domain);
+console.log(document.URL);
+console.log(document.title);
+
+// document.title = "changer";
+console.log(document.doctype);
+// console.log(document.head);
+console.log(document.body);
+console.log(document.all[10]);
+// console.log(document.body);
+
+// document.all[21].textContent= "Hello"; //this replaces the inner textContent
+
+console.log(document.forms);    //this gives array of all forms
+console.log(document.links);    //array of all links
+console.log(document.images);   //array of all images
+
+
+console.log(document.getElementById('header-title')) //searches for id header-title and gives everything in the tage
+var headerTitle = document.getElementById('header-title');
+console.log(headerTitle);
+// headerTitle.textContent= "Hello";
+document.getElementById("header-title").textContent= "yoyo"; //changes current html
+
+// headerTitle.style.borderBottom= "solid 3px #000"; //this is how you access or add css promperties
